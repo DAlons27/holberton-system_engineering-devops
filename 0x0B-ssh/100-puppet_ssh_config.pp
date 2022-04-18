be0092a416227545496f031d8af0f26468057174
DAlons27 <alonso.morey.arq@gmail.com>
2022-04-17 22:08:58 -0500

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 IntelliJ IDEAPyCharmRubyMine   
 # Configurate SSH to connect to a server without typing a password.
 
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  path =>  '/etc/ssh/ssh_config',
-  line =>  'PasswordAuthentication no',
+file_line { 'identity_file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '	IdentityFile ~/.ssh/school',
 }
 
-file_line {  'Declare identity file':
-  path =>  '/etc/ssh/ssh_config',
-  line =>  'IdentityFile ~/.ssh/school',
+file_line { 'password_no':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '	PasswordAuthentication no',
 }
