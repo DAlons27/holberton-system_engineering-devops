654a62e82e5dd6aaea3324b0a38ad81d608ae3ab
DAlons27 <alonso.morey.arq@gmail.com>
2022-04-19 23:24:26 -0500

    Install Nginx web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,27 @@
 # Puppet script that configures a "nginx" web server in an ubuntu 16.04 machine
 
 package { 'nginx':
-  ensure => installed,
-  name   => 'nginx',
+  ensure => installed
+}
+
+file_line { 'Add redirection, 301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Holberton School for the win!',
-  path    => '/var/www/html/index.html'
+  ensure  => file,
+  content => 'Hello World'
 }
 
-file_line { 'title':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'rewrite ^/redirect_me https://juanfe9118.github.io/Bass-Love/ permanent;',
-  multiple => true
-}
+#file { '/usr/share/nginx/html/404.html':
+#  ensure  => file,
+#  path    => '/usr/share/nginx/html/404.html',
+#  mode    => '0644'
+#  content => 'Ceci n\'est pas une page'
+#}
 
 service { 'nginx':
   ensure  => running,
