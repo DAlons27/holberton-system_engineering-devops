723a5ae5ce222c171b762d83abf70f8c6af0f6aa
DAlons27 <alonso.morey.arq@gmail.com>
2022-05-31 23:46:19 -0500

    Count it

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,37 +1,94 @@
 #!/usr/bin/python3
-''' task 2 module'''
-
+"""
+Recurse it
+"""
 import requests
 
 
-def count_words(subreddit, word_list, found_list=[], after=None):
-    '''return count of keywords in hot posts titles'''
-    user_agent = {'User-agent': 'test45'}
-    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
-                         .format(subreddit, after), headers=user_agent)
-    if after is None:
-        word_list = [word.lower() for word in word_list]
+def matches_words(list_posts, dict_words):
+    """Function that counts the matches with the keywords.
+    """
+    for post in list_posts:
+        for word in post.get('data').get('title').split():
+            for key in dict_words.keys():
+                if key.lower() == word.lower():
+                    dict_words[key] += 1
 
-    if posts.status_code == 200:
-        posts = posts.json()['data']
-        aft = posts['after']
-        posts = posts['children']
-        for post in posts:
-            title = post['data']['title'].lower()
-            for word in title.split(' '):
-                if word in word_list:
-                    found_list.append(word)
-        if aft is not None:
-            count_words(subreddit, word_list, found_list, aft)
-        else:
-            result = {}
-            for word in found_list:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            for key, value in sorted(result.items(), key=lambda item: item[1],
-                                     reverse=True):
-                print('{}: {}'.format(key, value))
-    else:
+
+def recurse(subreddit, dict_words, after=None):
+    """Recursive function that queries the Reddit API and
+    search keywords in titles posts.
+    """
+    api_header = {'User-Agent': 'Mozilla/5.0'}
+    api_params = {'after': after}
+    api_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    api_res = requests.get(
+        api_url,
+        headers=api_header,
+        params=api_params
+    )
+    if api_res.status_code != 200:
+        return(None)
+
+    api_data = api_res.json().get('data')
+    
+    list_posts = api_data.get('children')
+    
+    after = api_data.get('after')
+
+    matches_words(list_posts, dict_words)
+
+    if after is None:
         return
+    recurse(subreddit, dict_words, after)
+
+def count_words(subreddit, word_list):
+    """Function that queries the Reddit API,
+    parses the title of all hot articles,
+    and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces.
+    Javascript should count as javascript,
+    but java should not).
+    """
+    dict_words = dict()
+    dict_clear = dict()
+
+    for word in word_list:
+        dict_clear[word.lower()] = 0
+        if word.lower() in dict_clear.keys():
+            if dict_words.get(word.lower()) is None:
+                dict_words[word.lower()] = 1
+            else:
+                dict_words[word.lower()] += 1
+
+    recurse(subreddit, dict_clear)
+
+    for k, v in dict_clear.items():
+        dict_clear[k] = v * dict_words[k]
+
+    list_sorted = sorted(dict_clear.items(), key=lambda item: item[1])
+    list_sorted.reverse()
+
+    dict_repeat = dict()
+    list_repeat = list()
+    current = 0
+    for dupla in list_sorted:
+        if current == 0:
+            current = dupla[1]
+        if current == dupla[1]:
+            list_repeat.append(dupla[0])
+            dict_repeat[current] = list_repeat
+        else:
+            current = dupla[1]
+            list_repeat = list()
+            list_repeat.append(dupla[0])
+            dict_repeat[current] = list_repeat
+
+    for k, v in dict_repeat.items():
+        if k != 0:
+            if len(v) > 1:
+                v.sort()
+                for w in v:
+                    print("{}: {}".format(w, k))
+            else:
+                print("{}: {}".format(v[0], k))
